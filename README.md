83591e31fceff95c722b2c84b7232867e004c398
lipika.manglaa <lipika.mangla.web@gmail.com>
2023-07-20 09:45:54 +1000

    done

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # weatherForcast
+
+# workSchedule
+
+This is a weather forcast  application using HTML, CSS, and Javascript. In this application ,user can search curent weather and next five days weather according to their search city. Even you don't need to search again and again if you already search because you can see searched city  in the left side,just click on it , see weather of your choice. if user can search same city, ,can see message that alrady exist  by Toast Plugin so its easier for user to not to type again , jsut click on left side searched cities. you can delete from search history if you want
+
+
+
+This project has been deployed to GitHub Pages. To get this project up and running, you can follow the deployment link. Or, download the sources files to use this as a template.
+
+* [GitHub Repository]
+* [Deployed GitHub IO]
+
+
+
+### Prerequisites
+
+To install this application, you will need a text editor. I recommend Visual Studio Code. 
+
+### Installing
+
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository. 
+
+
+
+##  User Story:
+**AS  need to know current weather and forcast next five days weather for everyone**
+- I WANT to see current and forcast weather
+- SO THAT I can manage according to weather
+
+**# This project has the following features: **
+- When user search the city
+  - current and forcast weather is displayed in the right side 
+
+  ![](screenshot/Screenshot2.png)
+
+
+- When user can see more features,
+  - They're presented with with temparture, windy, humidity , how it looks like ,image can show cloudy,rainy,sunny etc.
+
+- When the user type again city name data 
+ -it can shows by Toast Plugin  ,city name already exists
+
+ ![](screenshot/Screenshot3.png)
+
+- If user already serach city
+  - Each time doesnt need to type again , just click on left side city which will be highlighted, to see weather
+ ![](screenshot/Screenshot4.png)
+
+- When the user does not type any city , just click on serach button 
+ -it can shows by Toast Plugin  ,please type city name
+
+ ![](screenshot/Screenshot5.png)
+ 
+
+ ### This project has media Queries for:
+
+* max-width: 991px 
+    * Adjusts body and container width
+
+* max-width: 767px
+    * Adjusts body and container width
+
+* max-width: 567px
+    * Adjusts body and container width  
--- js/script.js
+++ js/script.js
@@ -5,52 +5,163 @@ let weatherContentInner = document.querySelector(".weather-right-city")
 let weatherFivedays = document.querySelector(".weather-content-inner");
 let cityListItems = document.querySelector(".history-city ul")
 
-weatherForm.addEventListener("submit",async (e) => {
+let finalDataLocal;
+//form Submission and data save in localStoage
+weatherForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    let oldCityData = e.target.weatherCityName.value;
-    displayDataByInput(oldCityData)
-//    await displayWeatherCity(oldCityData) 
+    let inputValueByUser = e.target.weatherCityName.value;
+  
+    //for currrent weather
+     displayDataByInput(inputValueByUser)
+     //forcast weather
+     displayforcastdataByCityName(inputValueByUser)
+   
+    // await displayWeatherCity(inputValueByUser)
+    //get data from local storage
     let oldLocalData = JSON.parse(localStorage.getItem("weather")) ?? [];
 
-   
-    //same two cities cannt enter
-    if (!oldLocalData.includes(oldCityData)) {
-        let newLocalData = [...oldLocalData, oldCityData];
+    //if user didnt put any city name, input box is empty
+    if (inputValueByUser != "") {
+        //same two cities cannt enter
+        if (!oldLocalData.includes(inputValueByUser)) {
+            let newLocalData = [...oldLocalData, inputValueByUser];
+
+              //set data into local storage
+            finalDataLocal = localStorage.setItem("weather", JSON.stringify(newLocalData))
+            
 
 
-       
+        }
 
-
-        let finalDataLocal = localStorage.setItem("weather", JSON.stringify(newLocalData))
-    //   await displayCity()
-  
-     
+        else {
+            $.toast({
+                text:"City Name already exists!!",
+                icon: 'info',
+                  position : 'top-left' ,
+                hideAfter : 2000, 
+                showHideTransition : 'slide'
+              })
+        }
+    } else {
+        $.toast({
+            text:"please type any City name!!",
+            icon: 'info',
+              position : 'top-left' ,
+            hideAfter : 2000, 
+            showHideTransition : 'slide'
+          })
     }
-
-    else {
-        alert("City Name already exists!!")
-    }
-
+    displayCity();
     e.target.weatherCityName.value = ""
-      
+
 
 })
 
 
-//Search city Name in publish here
+//History Searched city Name in  here
 function displayCity() {
+    cityListItems.innerHTML='';
     let getDataLocal = JSON.parse(localStorage.getItem("weather")) ?? []
     getDataLocal.forEach((value, index) => {
-        cityListItems.innerHTML += `<li onclick="activeItem(${index}) ; displayWeatherCity(${index})">${value}</li>`
-
+        cityListItems.innerHTML += `<li>  <span  onclick="activeItem(${index}) ; displayDataByInput('${value}'), displayforcastdataByCityName('${value}')">${value}</span><span onclick="deleteItemsList(${index})"><i class="fa-solid fa-delete-left"></i></span></li>`
     });
 
 }
 displayCity()
 
 
+
+
+
+//current display weather from input when we search input box
+async function displayDataByInput(cityName) {
+   
+    const apiKey = `4d8fb5b93d4af21d66a2948710284366`;
+    let iconPath = `http://openweathermap.org/img/w/`
+
+    //current weather
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    let fetchData = await fetch(url);
+    let finalData = await fetchData.json();
+
+    let { main, sys, weather, wind, name } = finalData
+
+    //today's Date
+    let currentDay = new Date();
+    let currentDate = currentDay.getDate();
+    let currentMonth = currentDay.getMonth() + 1;
+    let currentyear = currentDay.getFullYear();
+    let todaySData = `${currentDate}/${currentMonth}/${currentyear}`
+
+    weatherContentInner.innerHTML = ` <div class="weather-city-show">
+                <h3>${name}  ${todaySData}</h3><img src="${iconPath+weather[0].icon}.png" />
+                <div class="temp">
+                    <h4>Temp:<span>${main.temp}<sup>o</sup>C</span></h4>
+                </div>
+                <div class="wind">
+                    <h4>Wind:<span>${wind.speed}MPH</span></h4>
+                </div>
+                <div class="humidity">
+                    <h4>Humidity:<span>${main.humidity}%</span></h4>
+                </div>
+                </div>`
+
+}
+
+
+    //five days weather according to user input
+
+    let timeZone;
+    let dayTime;
+
+     async function displayforcastdataByCityName(cityName){
+    let iconPath = `http://openweathermap.org/img/w/`
+
+    const urlFiveDays = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lat={lat}&lon={lon}&appid=72b27c2c885152dc7727a5905cfe710c`
+    let fetchDataFiveDays = await fetch(urlFiveDays);
+    let finalDataFiveDays = await fetchDataFiveDays.json();
+    let filterData = finalDataFiveDays.list;
+
+    weatherFivedays.innerHTML = ""
+    filterData.forEach((v, i) => {
+
+        //time in API
+        var timeData = v.dt_txt
+       
+            //want to get last time which is "00:00:00"
+            timeZone = timeData.split(" ")[1]
+            dayTime = timeData.split(" ")[0]
+       
+
+        //just need data for daily , not for hourly data
+        if (timeZone == "00:00:00") {
+
+          weatherFivedays.innerHTML += ` <div class="weather-city-show">
+                <h3>${cityName}  ${dayTime}</h3><img src="${iconPath + v.weather[0].icon}.png" />
+                <div class="temp">
+                    <h4>Temp:<span>${(v.main.temp - 273.15).toFixed(2)}<sup>o</sup>C</span></h4>
+                </div>
+                <div class="wind">
+                    <h4>Wind:<span>${v.wind.speed}MPH</span></h4>
+                </div>
+                <div class="humidity">
+                    <h4>Humidity:<span>${v.main.humidity}%</span></h4>
+                </div>
+                </div>`
+        }
+    })
+
+}
+
+
+    
+
+
 //active List items means history city
+
+
 function activeItem(index) {
+   
     let activeItem = document.querySelectorAll(".history-city ul li")
     activeItem.forEach((v, i) => {
         if (index != i) {
@@ -64,78 +175,28 @@ function activeItem(index) {
 }
 
 
+//active item hover  form local storage if have , then remove by input value
+let userrInput=document.querySelector("#userrInput");
+let activeItemList = document.querySelectorAll(".history-city ul li ")
+userrInput.addEventListener("keyup",()=>{
+    activeItemList.forEach((v,i)=>{
+        activeItemList[i].classList.remove("activeListItems")
+    })
+})
 
 
-//display weather accoding to input city;
-async function displayWeatherCity(index) {
+//delete item for history items
 
-    const apiKey = `4d8fb5b93d4af21d66a2948710284366`;
-    let iconPath = `http://openweathermap.org/img/w/`
-    
-    let getLocalStorageData=JSON.parse(localStorage.getItem("weather"))  ?? []
+let activeItemListDelete = document.querySelectorAll(".history-city ul li")
+function deleteItemsList(indexWeather){
 
-    for(var s in getLocalStorageData){
-        
-    
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${getLocalStorageData[s]}&appid=${apiKey}&units=metric`;
-    let fetchData = await fetch(url);
-    let finalData = await fetchData.json();
 
-    let { main, sys, weather, wind, name } = finalData
-   
-    //today's Date
-    let currentDay = new Date();
-    let currentDate = currentDay.getDate();
-    let currentMonth = currentDay.getMonth() + 1;
-    let currentyear = currentDay.getFullYear();
-    let todaySData = `${currentDate}/${currentMonth}/${currentyear}`
-    if (index==s) {
-        weatherContentInner.innerHTML = ` <div class="weather-city-show">
-                <h3>${name}  ${todaySData}</h3><img src="" />
-                <div class="temp">
-                    <h4>Temp:<span>${main.temp}<sup>o</sup>F</span></h4>
-                </div>
-                <div class="wind">
-                    <h4>Wind:<span>${wind.speed}MPH</span></h4>
-                </div>
-                <div class="humidity">
-                    <h4>Humidity:<span>${main.humidity}%</span></h4>
-                </div>
-                </div>`
+        let localGetDataNew = JSON.parse(localStorage.getItem("weather")) ?? []
+        localGetDataNew.splice(indexWeather,1)
+        finalDataLocal = localStorage.setItem("weather", JSON.stringify(localGetDataNew))
+        weatherContentInner.innerHTML=""
+        weatherFivedays.innerHTML = "";
+      displayCity();
     }
-   }
-}
+  
 
-// displayWeatherCity()
-
-
-async function displayDataByInput(cityName){
-    const apiKey = `4d8fb5b93d4af21d66a2948710284366`;
-    let iconPath = `http://openweathermap.org/img/w/`
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-    let fetchData = await fetch(url);
-    let finalData = await fetchData.json();
-
-    let { main, sys, weather, wind, name } = finalData
-   
-    //today's Date
-    let currentDay = new Date();
-    let currentDate = currentDay.getDate();
-    let currentMonth = currentDay.getMonth() + 1;
-    let currentyear = currentDay.getFullYear();
-    let todaySData = `${currentDate}/${currentMonth}/${currentyear}`
-   
-        weatherContentInner.innerHTML = ` <div class="weather-city-show">
-                <h3>${name}  ${todaySData}</h3><img src="" />
-                <div class="temp">
-                    <h4>Temp:<span>${main.temp}<sup>o</sup>F</span></h4>
-                </div>
-                <div class="wind">
-                    <h4>Wind:<span>${wind.speed}MPH</span></h4>
-                </div>
-                <div class="humidity">
-                    <h4>Humidity:<span>${main.humidity}%</span></h4>
-                </div>
-                </div>`
-    }
